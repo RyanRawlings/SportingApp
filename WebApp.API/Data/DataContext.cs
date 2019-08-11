f1353fba8d325841739e486514969aa33e110e05
RyanRawlings <ryan.rawlings96@gmail.com>
2019-08-11 22:07:37 +1000

    Initial Commit

---
+++ WebApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using WebApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values {get; set; }
+    }
+}
